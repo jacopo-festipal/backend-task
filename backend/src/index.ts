bfd3cb38c92dbf742fce22f1af7685bd63e030bb
thisisjacopo <jacopo.devs@icloud.com>
2025-11-11 13:44:21 +0100

    general fix:posting and receiving chat messages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ config(); // Loads .env file into process.env
 
 import app from "./app";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env["PORT"] || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
